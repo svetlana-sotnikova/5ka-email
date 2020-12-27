da6bf728a097906fa645f2b3d4c8039721e89dba
Svetlana Sotnikova <svetlana.data@gmail.com>
2020-12-27 16:44:38 +0300

    Правка Info

--- src/containers/Email/Info/Info.jsx
+++ src/containers/Email/Info/Info.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { Button, Image, Paragraph } from "../../../components";
-import bonus from "./bonus.png";
+import React from 'react';
+import { Button, Image, Paragraph } from '../../../components';
+import bonus from './bonus.png';
 
 export const Info = () => (
   <>
-    <div style={{ textAlign: "center", padding: "8px 30px 57px" }}>
-      <Paragraph style={{ fontSize: 25, fontWeight: "bold" }}>
+    <div style={{ textAlign: 'center', padding: '8px 30px' }}>
+      <Paragraph style={{ fontSize: 25, fontWeight: 'bold' }}>
         Как использовать баллы
       </Paragraph>
 
-      <Paragraph style={{ padding: "20px 0" }}>
+      <Paragraph style={{ padding: '20px 0' }}>
         Вы можете списывать баллы при&nbsp;совершении покупок в&nbsp;«Пятерочке»
         для&nbsp;получения скидок до&nbsp;100% от&nbsp;суммы чека!<sup>1</sup>
       </Paragraph>
@@ -17,11 +17,11 @@ export const Info = () => (
       <Image
         src={bonus}
         alt="Баллы"
-        style={{ display: "inline-block", width: "100%", padding: "5px 0" }}
+        style={{ display: 'inline-block', width: '100%', padding: '5px 0' }}
       />
     </div>
 
-    <div style={{ padding: "34px 0", textAlign: "center" }}>
+    <div style={{ padding: '25px 0 92px', textAlign: 'center' }}>
       <Button href="https://5ka.ru/" style={{ width: 210 }}>
         Узнать баланс карты
       </Button>
